3814daf1d17be385464f810c60292459076697f3
Felipe Espinoza <domingues.espinoza@outlook.com>
2023-08-22 10:17:52 -0300

    Criando ação de Editar descrição de tarefa e Salvar

--- src/components/Tarefa/index.tsx
+++ src/components/Tarefa/index.tsx
@@ -1,16 +1,35 @@
-import { remover } from '../../store/reducers/tarefas'
+import { remover, editar } from '../../store/reducers/tarefas'
 import { useDispatch } from 'react-redux'
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import * as S from './styles'
 
 import TarefaClass from '../../models/Tarefa'
 
 type Props = TarefaClass
 
-const Tarefa = ({ descricao, prioridade, status, titulo, id }: Props) => {
+const Tarefa = ({
+  descricao: descricaoOriginal,
+  prioridade,
+  status,
+  titulo,
+  id
+}: Props) => {
   const dispatch = useDispatch()
   const [estaEditando, setEstaEditando] = useState(false)
+
+  const [descricao, setDescricao] = useState('')
+
+  useEffect(() => {
+    if (descricaoOriginal.length > 0) {
+      setDescricao(descricaoOriginal)
+    }
+  }, [descricaoOriginal])
+
+  function cancelarEdicao() {
+    setEstaEditando(false)
+    setDescricao(descricaoOriginal)
+  }
 
   return (
     <S.Card>
@@ -22,14 +41,32 @@ const Tarefa = ({ descricao, prioridade, status, titulo, id }: Props) => {
         {status}
       </S.Tag>
       <S.SubTitulo>Detalhes:</S.SubTitulo>
-      <S.Descricao maxLength={572} value={descricao} />
+      <S.Descricao
+        disabled={!estaEditando}
+        maxLength={572}
+        value={descricao}
+        onChange={(evento) => setDescricao(evento.target.value)}
+      />
       <S.BarraAcoes>
         {estaEditando ? (
           <>
-            <S.BotaoSalvar>
+            <S.BotaoSalvar
+              onClick={() => {
+                dispatch(
+                  editar({
+                    descricao,
+                    prioridade,
+                    status,
+                    titulo,
+                    id
+                  })
+                )
+                setEstaEditando(false)
+              }}
+            >
               <i className="bi bi-plus-lg" title="Salvar"></i>
             </S.BotaoSalvar>
-            <S.BotaoDelete onClick={() => setEstaEditando(false)}>
+            <S.BotaoDelete onClick={cancelarEdicao}>
               <i className="bi bi-x-lg" title="Cancelar"></i>
             </S.BotaoDelete>
           </>
